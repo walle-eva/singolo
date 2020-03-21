4eb83698a893fb151b193794ae7200fbef06d447
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 04:31:30 +0700

    fix: naming, styles

--- script.js
+++ script.js
@@ -1,21 +1,127 @@
-//header
-document.querySelector('.navigation__items').addEventListener('click', (event) => {
-  event.preventDefault();
-  document.querySelectorAll('.navigation__item').forEach((item) => {
-    item.classList.remove('navigation__item_active')
-  });
-  event.target.closest('li').classList.add('navigation__item_active');
-  
-  let marginMenu = getComputedStyle(document.querySelector('.navigation__items'));	
-  let blockID = event.target.closest('a').getAttribute('href').substr(1);
-  
-  window.scrollTo({
-	top: document.getElementById(blockID).getBoundingClientRect().top + pageYOffset - parseInt(marginMenu.height),
-	left: 0,
-	behavior: 'smooth'})
-});
+let data = [
+    {
+        id: 1,
+		idToImg: 'Project1',
+        title: 'Project1',
+        urlToImage: 'assets/img/portfolio/Project.png',
+    },
+    {
+       id: 2,
+		idToImg: 'Project2',
+        title: 'Project2',
+        urlToImage: 'assets/img/portfolio/Project2.png',
+    },
+    {
+       id: 3,
+		idToImg: 'Project3',
+        title: 'Project3',
+        urlToImage: 'assets/img/portfolio/Project3.png',
+    },
+	{
+       id: 4,
+		idToImg: 'Project4',
+        title: 'Project4',
+        urlToImage: 'assets/img/portfolio/Project4.png',
+    },
+	{
+       id: 5,
+		idToImg: 'Project5',
+        title: 'Project5',
+        urlToImage: 'assets/img/portfolio/Project5.png',
+    },
+	{
+       id: 6,
+		idToImg: 'Project6',
+        title: 'Project6',
+        urlToImage: 'assets/img/portfolio/Project6.png',
+    },
+	{
+       id: 7,
+		idToImg: 'Project7',
+        title: 'Project7',
+        urlToImage: 'assets/img/portfolio/Project7.png',
+    },
+	{
+       id: 8,
+		idToImg: 'Project8',
+        title: 'Project8',
+        urlToImage: 'assets/img/portfolio/Project8.png',
+    },
+	{
+       id: 9,
+		idToImg: 'Project9',
+        title: 'Project9',
+        urlToImage: 'assets/img/portfolio/Project9.png',
+    },
+	{
+       id: 10,
+		idToImg: 'Project10',
+        title: 'Project10',
+        urlToImage: 'assets/img/portfolio/Project10.png',
+    },
+	{
+       id: 11,
+		idToImg: 'Project11',
+        title: 'Project11',
+        urlToImage: 'assets/img/portfolio/Project11.png',
+    },
+	{
+       id: 12,
+		idToImg: 'Project12',
+        title: 'Project12',
+        urlToImage: 'assets/img/portfolio/Project12.png',
+    }
+];
 
-document.addEventListener('scroll', (event) => {
+
+
+window.onload = function() {
+	//Header
+	
+    // Menu click
+    addMenuClickHandler();
+
+    // Menu Scroll
+    addMenuScrollHandler();
+	
+	//slider
+	addSliderClickHandler()
+	
+	//Portfolio
+	
+	//Tags
+	renderImgsToDom();
+	
+	//BorderImg
+	addBorderImg()
+	
+	//Contacts
+	addSendClickHandler();
+}
+
+const addMenuClickHandler = () => {
+	let navigationItems = document.querySelector('.navigation__items');
+	
+	navigationItems.addEventListener('click', (event) => {
+	event.preventDefault();
+	
+	document.querySelectorAll('.navigation__item').forEach((item) => {
+      item.classList.remove('navigation__item_active')
+    });
+	event.target.closest('li').classList.add('navigation__item_active');
+  
+	let marginMenu = getComputedStyle(navigationItems);	
+	let blockID = event.target.closest('a').getAttribute('href').substr(1);
+  
+	window.scrollTo({
+	  top: document.getElementById(blockID).getBoundingClientRect().top + pageYOffset - parseInt(marginMenu.height),
+	  left: 0,
+	  behavior: 'smooth'})
+	});
+}
+
+const addMenuScrollHandler = () => {
+	document.addEventListener('scroll', (event) => {
 	  let currentPos = window.scrollY;
 	  let sections= document.querySelectorAll('section');
 	  let links = document.querySelectorAll('.navigation__item a');
@@ -32,9 +138,12 @@ document.addEventListener('scroll', (event) => {
 		  }
 	  })	
 	})
+}
+
 
 
 //slider
+const addSliderClickHandler = () => {
 let items = document.querySelectorAll('.item-slider');
 let currentItem = 0;
 let isEnabled = true;
@@ -99,94 +208,248 @@ function onScreen1(event){
 	}
 }
 
+}
 
-//portfolio
-function randomInteger(min, max, num) {
+const generateImgs = (data) => {
+    let imgs = [];
+    data.forEach((itemImg) => {
+		let img = document.createElement('img');
+        img.setAttribute('id', itemImg.id);
+		img.setAttribute('src', itemImg.urlToImage);
+		img.setAttribute('alt', itemImg.title);
+		imgs.push(img)
+    });
+    return imgs;
+}
+
+const getImgWrapper = () => {
+    let imgWrapper = document.querySelector('.layout-4-column');
+    imgWrapper.innerHTML = '';
+	return imgWrapper;
+}
+
+const renderImgsToDom = () => { 
+   let imgWrapper = getImgWrapper();
+    generateImgs(data).forEach(item => {
+		let imgDiv = document.createElement('div');
+		imgDiv.classList.add('portfolio__img');
+        imgWrapper.append(imgDiv);
+		imgDiv.append(item);
+    })
+    addPortfolioClickHandler();
+}
+
+const addPortfolioClickHandler = () => {
+	document.querySelector('.tag').addEventListener('click', (event) => {
+	event.preventDefault();
+	document.querySelectorAll('.tags').forEach((item) => {
+		item.classList.remove('tag__active');
+	});
+	event.target.closest('li').classList.add('tag__active');
+	
+	let img = document.querySelectorAll('.portfolio__img');
+	let arrRandom = [];
+	arrRandom = randomInteger(0, img.length - 1, img.length)
+	
+	newData = [];
+	
+	for(let i = 0; i < arrRandom.length; i++){
+	  newData.push(data[arrRandom[i]]);
+	}	
+	data = newData;
+	renderImgsToDom();
+})
+}
+
+const randomInteger = (min, max, num) => {
   var i, arr = [], res = [];
 	    for (i = min; i <= max; i++ ) arr.push(i);
 	    for (i = 0; i < num; i++) res.push(arr.splice(Math.floor(Math.random() * (arr.length)), 1)[0])
 	    return res;
 }
 
-document.querySelector('.teg').addEventListener('click', (event) => {
-	event.preventDefault();
-	document.querySelectorAll('.tegs').forEach((item) => {
-		item.classList.remove('teg__active');
-	});
-	event.target.closest('li').classList.add('teg__active');
-	
-	let img = document.querySelectorAll('.portfolio__img');
-	let arrRandom = [];
-	arrRandom = randomInteger(0, img.length, img.length)
-	
-	for(let i = 0; i < arrRandom.length; i++){
-		img[i].style.order = arrRandom[i];
-	}	
-})
-
-document.querySelector('.layout-4-column').addEventListener('click', () => {
+const addBorderImg = () => {
+	document.querySelector('.layout-4-column').addEventListener('click', () => {
 	document.querySelectorAll('.portfolio__img').forEach((item) => {
 		item.classList.remove('img_active');
 	});
 	event.target.closest('.portfolio__img').classList.add('img_active');
 })
+}
 
-//contact
-document.querySelector('button').addEventListener('click', () => {
+
+const addSendClickHandler = () => {
+	document.querySelector('button').addEventListener('click', () => {
 	event.preventDefault();
-let windowMessage = document.createElement('div');
-windowMessage.classList.add('modal');
+	renderModalWindow();
+	validateModalWindow();
+	closeModalWindow();
+}
+)}
 
-let subjectValue = document.forms.contact.elements.subject.value;
-let describeValue = document.forms.contact.elements.message.value;
-let subject = '';
-let describe = '';
-if(subjectValue == 'Singolo'){
-	subject = 'Тема: Singolo';
-}
-else if(subjectValue == ''){
-	subject = 'Без темы';
-}
-else {
-	subject = 'Тема: ' + subjectValue;
-}
+const generateModalWindow = () => {
+	let windowMessage = document.createElement('div');
+	windowMessage.classList.add('modal');
 
-if(describeValue == 'Singolo'){
-	describe = 'Описание: Portfolio project ';
-}
-else if(describeValue == ''){
-	describe= 'Без описания';
-}
-else {
-	describe = 'Описание: ' + describeValue;
-}
+	let subjectValue = document.forms.contact.elements.subject.value;
+	let describeValue = document.forms.contact.elements.message.value;
+	let subject = '';
+	let describe = '';
+	if(subjectValue == 'Singolo'){
+		subject = 'Тема: Singolo';
+	}
+	else if(subjectValue == ''){
+		subject = 'No subject';
+	}
+	else {
+		subject = 'Subject: ' + subjectValue;
+	}
 
-if(!document.forms.contact.elements.name.validity.valid){
-	document.forms.contact.elements.name.style.border = '1px solid #fff';
-	document.querySelector('.hint-name').classList.add('hint_active');
-} else {
-	document.forms.contact.elements.name.style.border = 'none';
-	document.querySelector('.hint-name').classList.remove('hint_active');
-}
-if(!document.forms.contact.elements.email.validity.valid){
-	document.forms.contact.elements.email.style.border = '1px solid #fff';
-	document.querySelector('.hint-email').classList.add('hint_active');
-}else{
-	document.forms.contact.elements.email.style.border = 'none';
-	document.querySelector('.hint-email').classList.remove('hint_active');
+	if(describeValue == 'Singolo'){
+		describe = 'Description: Portfolio project ';
+	}
+	else if(describeValue == ''){
+		describe= 'No description ';
+	}
+	else {
+		describe = 'Описание: ' + describeValue;
+	}
+	
+	windowMessage.innerHTML = `<p>The letter was sent </p>  <p>${subject}</p> <p>${describe}</p> <button class="button-modal">OK</button>`;
+	return windowMessage;
+
 }
 
-windowMessage.innerHTML = `<p>Письмо отправлено </p>  <p>${subject}</p> <p>${describe}</p> <button class="button-modal">OK</button>`;
-if(document.forms.contact.elements.email.validity.valid && document.forms.contact.elements.name.validity.valid){
-document.body.appendChild(windowMessage);
-} else {
-	return;
+const renderModalWindow = () => {
+	if(document.forms.contact.elements.email.validity.valid && document.forms.contact.elements.name.validity.valid){
+	document.body.append(generateModalWindow());
+	} else {
+		return;
+	}
 }
 
-document.querySelector('.button-modal').addEventListener('click', () => {
-	document.querySelector('.modal').remove();
-})
-});
+const closeModalWindow = () => {
+	document.querySelector('.button-modal').addEventListener('click', () => {
+		document.querySelector('.modal').remove();
+	})
+	
+	clearForm();
+}
+
+const clearForm = () => {
+	let formValue = document.forms.contact.elements;
+	[].forEach.call(formValue,(value) => {
+		value.value = '';
+	})
+}
+
+const validateModalWindow = () => {
+	if(!document.forms.contact.elements.name.validity.valid){
+		document.forms.contact.elements.name.style.border = '1px solid #fff';
+		document.querySelector('.hint-name').classList.add('hint_active');
+	} else {
+		document.forms.contact.elements.name.style.border = 'none';
+		document.querySelector('.hint-name').classList.remove('hint_active');
+	}
+	if(!document.forms.contact.elements.email.validity.valid){
+		document.forms.contact.elements.email.style.border = '1px solid #fff';
+		document.querySelector('.hint-email').classList.add('hint_active');
+	}else{
+		document.forms.contact.elements.email.style.border = 'none';
+		document.querySelector('.hint-email').classList.remove('hint_active');
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
